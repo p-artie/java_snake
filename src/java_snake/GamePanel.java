ee07c8a397dacf8e3e0fd0c63c25cce14dfb8576
p-artie <artiepolanco@gmail.com>
2022-05-24 17:23:19 -0400

    Change delay constant

--- src/java_snake/GamePanel.java
+++ src/java_snake/GamePanel.java
@@ -20,7 +20,7 @@ public class GamePanel extends JPanel implements ActionListener{
 	static final int SCREEN_HEIGHT = 600;
 	static final int UNIT_SIZE = 25;
 	static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;
-	static final int DELAY = 150;
+	static final int DELAY = 75;
 	final int x[] = new int[GAME_UNITS];
 	final int y[] = new int[GAME_UNITS];
 	int bodyParts = 6;
